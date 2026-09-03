c36f5eb1de4366e1f113b964a2a3f015975654ec
bekzat3348-ui <bekzat3348@gmail.com>
2026-09-03 14:11:01 +0500

    asdas

--- task1.py
+++ task1.py
@@ -1,1 +1,11 @@
-print('bekzat')
+numbers = [4, 7, 2, 9, 12, 5, 8, 3]
+total = 0
+even_numbers = []
+
+for number in numbers:
+    if number % 2 == 0:
+        even_numbers.append(number)
+        total += number ** 2
+
+print("even_numbers:", even_numbers)
+print("total:", total)
